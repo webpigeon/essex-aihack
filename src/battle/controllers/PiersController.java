9cc651ede33f18b04c014655593861a6a71c5738
Piers <pwillic@essex.ac.uk>
2015-06-11 13:56:59 +0100

    Starting my controller

---
+++ src/battle/controllers/PiersController.java
@@ -0,0 +1,16 @@
+package battle.controllers;
+
+import asteroids.Action;
+import battle.BattleController;
+import battle.SimpleBattle;
+
+/**
+ * Created by pwillic on 11/06/2015.
+ */
+public class PiersController implements BattleController {
+
+    @Override
+    public Action getAction(SimpleBattle gameStateCopy, int playerId) {
+        return null;
+    }
+}
