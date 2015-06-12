5165a272170625eba25d908c597245d84b4dbfc0
Memo Akten <memo@memo.tv>
2015-06-12 10:53:46 +0100

    controller utils update

--- src/battle/controllers/Memo/MemoControllerUtils.java
+++ src/battle/controllers/Memo/MemoControllerUtils.java
@@ -23,16 +23,15 @@ public class MemoControllerUtils {
         }
     }
 
-    // fills in Action with turn and thrust
-    static double thrustTo(Vector2d s, Vector2d d, Vector2d desired_pos, double dist_threshold, double rot_threshold, Action action) {
+    // fills in Action with turn, returns whether we or not we've reached target
+    static boolean thrustTo(Vector2d s, Vector2d d, Vector2d desired_pos, double dist_threshold, double rot_threshold, Action action) {
         double dist_to_desired_pos = s.dist(desired_pos);
         if(dist_to_desired_pos < dist_threshold) {
-            action.thrust = 0;
+            return true;
         } else {
-            action.thrust = 1;
             action.turn = lookAt(s, d, desired_pos, rot_threshold);
+            return false;
         }
-        return action.thrust;
     }
 
 }
